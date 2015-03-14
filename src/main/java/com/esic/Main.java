b9621347af9331bb56099604fb75e328ab97c0ef
meeththakkar <meet.h.thakkar@gmail.com>
2015-03-15 03:14:55 +0530

    General : sample code for main class

--- src/main/java/com/esic/Main.java
+++ src/main/java/com/esic/Main.java
@@ -1,12 +1,17 @@
 package com.esic;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
 		System.out.println("Hello...");
+	//	Thread.sleep(1000000);
+		
+		JOptionPane.showConfirmDialog(null,"Hello");
 	}
 }
 
