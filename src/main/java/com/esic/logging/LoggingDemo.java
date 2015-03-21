1cdda877773b49cdf529762ea5bbd500799b9130
meeththakkar <meet.h.thakkar@gmail.com>
2015-03-21 15:46:27 +0530

    Logging Demo

---
+++ src/main/java/com/esic/logging/LoggingDemo.java
@@ -0,0 +1,18 @@
+package com.esic.logging;
+
+import org.apache.log4j.Logger;
+
+public class LoggingDemo {
+
+	
+	final static Logger logger = Logger
+			.getLogger(LoggingDemo.class);
+
+	
+	
+	
+	public static void main(String[] args) {
+		logger.info("Hello");
+	}
+	
+}
