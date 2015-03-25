ae7a0f5bd2526575203861de774bc4e5b52be847
meeththakkar <meet.h.thakkar@gmail.com>
2015-03-25 11:46:00 +0530

    boject store ( instead of spring )

--- src/main/java/com/esic/ObjectStore.java
+++ src/main/java/com/esic/ObjectStore.java
@@ -1,18 +1,49 @@
 package com.esic;
 
 import com.esic.Dao.ESICExcelDAO;
+import com.esic.processor.ESICProcessor;
+import com.esic.processor.ESICRecordProcessor;
 import com.esic.ui.UI;
 
 public class ObjectStore {
 
-	
-	
-	
-	public static UI ui;
-	
-	public static ESICExcelDAO excelDAO;
-	
-	
-	
-	public static String fileName;
+	static UI ui;
+
+	static ESICExcelDAO excelDAO;
+
+	static ESICProcessor processor;
+
+	static ESICRecordProcessor recordProcessor;
+
+	public static ESICRecordProcessor getRecordProcessor() {
+
+		if (recordProcessor == null) {
+			recordProcessor = new ESICRecordProcessor();
+		}
+		return recordProcessor;
+	}
+
+	public static ESICExcelDAO getExcelDAO() {
+
+		if (excelDAO == null) {
+			excelDAO = new ESICExcelDAO();
+		}
+		return excelDAO;
+	}
+
+	public static ESICProcessor getProcessor() {
+
+		if (processor == null) {
+			processor = new ESICProcessor();
+		}
+		return processor;
+	}
+
+	public static UI getUI() {
+		if (ui == null) {
+			ui = new UI();
+		}
+		return ui;
+	}
+
 }
